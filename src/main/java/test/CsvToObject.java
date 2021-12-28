576c4125421ee2687c7cbb5c6ea501159ffe70e9
mohamed <mohamed.ghalmi@efrei.net>
2021-12-28 15:35:57 +0100

    Final version

--- src/main/java/test/CsvToObject.java
+++ src/main/java/test/CsvToObject.java
@@ -10,29 +10,22 @@ import java.io.StringReader;
 public class CsvToObject {
 
     @SneakyThrows
-    public Song Song(String o) {
+    public Song Song(String song) {
 
-        CSVReader reader = new CSVReader(new StringReader(o));
+        CSVReader reader = new CSVReader(new StringReader(song));
         String[] parts = reader.readNext();
 
-        return Song.builder()
-                .id(parts[0])
-                .title(parts[1])
-                .date(Integer.parseInt(parts[2]))
-                .listening((parts[3]))
+        return Song.builder().id(parts[0]).title(parts[1]).date(Integer.parseInt(parts[2])).listening((parts[3]))
                 .build();
     }
 
     @SneakyThrows
-    public Singer Singer(String o) {
+    public Singer Singer(String singer) {
 
-        CSVReader reader = new CSVReader(new StringReader(o));
+        CSVReader reader = new CSVReader(new StringReader(singer));
         String[] parts = reader.readNext();
 
-        return Singer.builder()
-                .label(parts[0])
-                .id(parts[1])
-                .name(parts[2])
+        return Singer.builder().label(parts[0]).id(parts[1]).name(parts[2])
                 .build();
     }
 }
